ef9d5e919bb80153d6b0febea037e6e40459c36d
Henrique <henrynyyls@gmail.com>
2024-02-02 17:08:34 -0300

    All fully working now

---
+++ servidor.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const aplicacao = express();
+
+const servidorHttp = http.createServer(aplicacao);
+const io = require('socket.io')(servidorHttp);
+
+io.addListener('connection', (socket) => {
+  console.log('um usuario conectou')
+  socket.addListener('nova mensagem', (msg) => {
+    io.emit('nova mensagem', msg)
+  })
+})
+
+aplicacao.use(express.static('public'))
+
+servidorHttp.listen(1000, '192.168.0.100');
